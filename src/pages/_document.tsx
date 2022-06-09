481c977cdbc7006e4609bd5b9c54d7b33a18ba23
Tuag <igortuag@gmail.com>
2022-06-09 07:21:12 -0300

    adjust document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
-import Document, { Html, Main, NextScript } from "next/document";
-import Head from "next/head";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Spectral:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body className="loading">
           <Main />
           <NextScript />
         </body>
@@ -20,3 +20,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
