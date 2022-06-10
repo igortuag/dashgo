9b4098578cd28f1c0e7e587dc480f9d8703c7f26
Tuag <igortuag@gmail.com>
2022-06-10 07:47:15 -0300

    add container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <h1>Hello</h1>
+    </Flex>
+  );
 }
