492d00b6a4dcff4192e4593eb33305a35a5b4f58
Tuag <igortuag@gmail.com>
2022-06-10 07:50:37 -0300

    add form structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, Input } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
-      <h1>Hello</h1>
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Input name="email" type="email" />
+        <Input name="password" type="password" />
+
+        <Button type="submit" mt="6">
+          Login
+        </Button>
+      </Flex>
     </Flex>
   );
 }
