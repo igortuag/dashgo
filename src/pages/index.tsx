64734a4ba4ca937b39703e5970477088a82a4a62
Tuag <igortuag@gmail.com>
2022-06-12 08:10:37 -0300

    add function to submit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 import { Input } from "../components/Form/Input";
 
 export default function SignIn() {
+  const { push } = useRouter();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    push("/dashboard");
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,6 +20,7 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit}
       >
         <Stack spacing={4}>
           <Input name="email" label="Email" type="email" />
