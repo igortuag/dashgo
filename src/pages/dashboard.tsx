d8da58a55d12a7f43c0bf34210e16d254ee9da85
Tuag <igortuag@gmail.com>
2022-06-15 08:25:58 -0300

    add char as a dynamic import

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,20 @@
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
+const options = {};
+
+const series = [
+  {
+    name: "Series 1",
+    data: [30, 40, 45, 50],
+  },
+];
 
 export default function Dashboard() {
   return (
@@ -20,6 +34,7 @@ export default function Dashboard() {
             <Text fontSize="lg" mb="4">
               Week subscribers
             </Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
           <Box p="8" bg="gray.800" borderRadius={8}>
             <Text fontSize="lg" mb="4">
