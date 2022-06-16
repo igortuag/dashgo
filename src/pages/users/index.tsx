246da42ed354b757a03e81c3b8d721d07d801c24
Tuag <igortuag@gmail.com>
2022-06-16 08:32:31 -0300

    add table of users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,19 @@
-import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
-import { RiAddLine } from "react-icons/ri";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 
@@ -22,11 +36,151 @@ export default function UserList() {
               size="sm"
               fontSize="sm"
               colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} />}
+              leftIcon={<Icon as={RiAddLine} fontSize={20} />}
             >
               Create New
             </Button>
           </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>User</Th>
+                <Th>Created at</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Igor Tuag</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      igortuag@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>June 1, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Igor Tuag</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      igortuag@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>June 1, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Igor Tuag</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      igortuag@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>June 1, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Igor Tuag</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      igortuag@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>June 1, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Igor Tuag</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      igortuag@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>June 1, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
         </Box>
       </Flex>
     </Box>
