b31155d4d251e7cf3c2331699117fe746b8147a8
Tuag <igortuag@gmail.com>
2022-06-16 08:24:39 -0300

    add heading to page users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 
@@ -9,6 +10,24 @@ export default function UserList() {
 
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Users
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+            >
+              Create New
+            </Button>
+          </Flex>
+        </Box>
       </Flex>
     </Box>
   );
