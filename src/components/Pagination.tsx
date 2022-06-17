4ce5a22b07ab0d9f69b74ba309764ca29fef792a
Tuag <igortuag@gmail.com>
2022-06-17 08:04:18 -0300

    add pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,54 @@
+import { Button, Stack } from "@chakra-ui/react";
+
+export function Pagination() {
+  return (
+    <Stack>
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{
+          bgColor: "pink.500",
+          cursor: "default",
+        }}
+      >
+        1
+      </Button>
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        bgColor="gray.700"
+        _hover={{
+          bgColor: "gray.500",
+        }}
+      >
+        2
+      </Button>
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        bgColor="gray.700"
+        _hover={{
+          bgColor: "gray.500",
+        }}
+      >
+        3
+      </Button>
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        bgColor="gray.700"
+        _hover={{
+          bgColor: "gray.500",
+        }}
+      >
+        4
+      </Button>
+    </Stack>
+  );
+}
