b1d1fe52118db1e11c815fb04bd45e4871002a5b
Tuag <igortuag@gmail.com>
2022-06-19 07:25:03 -0300

    add form inputs to create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,12 @@
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 
@@ -10,7 +18,34 @@ export default function UserCreate() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8"></Box>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Create User
+          </Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Name" placeholder="Full name" />
+              <Input
+                name="email"
+                label="Email"
+                type="email"
+                placeholder="User email"
+              />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" label="Password" type="password" />
+              <Input
+                name="password_confirmation"
+                label="Password Confirmation"
+                type="password"
+              />
+            </SimpleGrid>
+          </VStack>
+        </Box>
       </Flex>
     </Box>
   );
