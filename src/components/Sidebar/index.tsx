d3ff4e53e1f5e92efbd0fdf5bf020f9bc74ad3a9
Tuag <igortuag@gmail.com>
2022-06-23 06:52:49 -0300

    add drawer sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,36 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 import SidebarNav from "./SidebarNav";
 
 export default function Sidebar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSidebar) {
+    <Drawer isOpen={true} placement="left" onClose={() => {}}>
+      <DrawerOverlay>
+        <DrawerContent>
+          <DrawerCloseButton mt="6" />
+          <DrawerHeader>Navigation</DrawerHeader>
+          <DrawerBody>
+            <SidebarNav />
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>;
+  }
+
   return (
     <Box as="aside" w="64" mr="8">
       <SidebarNav />
