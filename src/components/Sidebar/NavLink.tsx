1a597ba169c2022585552d39fe02703fda941fc0
Tuag <igortuag@gmail.com>
2022-06-27 07:10:21 -0300

    add next link to navLink

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,22 @@
-import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import React, { ElementType } from "react";
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
 function NavLink({ icon, children, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" alignItems="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
+    <Link href={href}>
+      <ChakraLink display="flex" alignItems="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
