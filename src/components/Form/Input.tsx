406555ec575f3e0353edbe714a3c7cceb4073059
Tuag <igortuag@gmail.com>
2022-06-30 07:35:01 -0300

    add ref to input

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,20 +4,24 @@ import {
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
-import { forwardRef } from "react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 }
 
-const InputBase = ({ name, label, ...rest }: InputProps) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, ...rest },
+  ref
+) => {
   return (
     <FormControl>
       {label && <FormLabel htmlFor="password">Password</FormLabel>}
       <ChakraInput
         name={name}
         id={name}
+        ref={ref}
         focusBorderColor="pink.500"
         bgColor="gray.900"
         variant="filled"
