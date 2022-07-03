e2efc4705f0cf05053df82b46d55565311cceaff
Tuag <igortuag@gmail.com>
2022-07-03 07:29:33 -0300

    add initial config to mirage with user entity

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,17 @@
+import { createServer, Model } from "miragejs";
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    routes() {},
+  });
+}
