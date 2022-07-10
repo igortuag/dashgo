bd9ad433ec35a0925cf021e1458d831f2349b14a
Tuag <igortuag@gmail.com>
2022-07-10 07:07:53 -0300

    disacople get users function

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,28 +1,25 @@
-import React from "react";
 import { useQuery } from "react-query";
 import { api } from "../api";
 
+export async function getUsers() {
+  const { data } = await api.get("/users");
+
+  const users = data.users.map((user) => ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    createdAt: new Date(user.created_at).toLocaleDateString("en-US", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    }),
+  }));
+
+  return users;
+}
+
 export default function useUsers() {
-  return useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("/users");
-
-      const users = data.users.map((user) => ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.created_at).toLocaleDateString("en-US", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      }));
-
-      return users;
-    },
-    {
-      staleTime: 5000,
-    }
-  );
+  return useQuery("users", getUsers, {
+    staleTime: 5000,
+  });
 }
