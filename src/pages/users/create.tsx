cba7c61b4058a8079579e40c714efe2e08c7e86e
Tuag <igortuag@gmail.com>
2022-07-17 07:04:17 -0300

    add invalidate query on success to refresh list

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -17,6 +17,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useMutation } from "react-query";
 import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   name: string;
@@ -41,16 +42,23 @@ const CreateUserFormSchema = yup.object().shape({
 });
 
 export default function UserCreate() {
-  const createUser = useMutation(async (user: CreateUserFormData) => {
-    const response = await api.post("users", {
-      user: {
-        ...user,
-        created_at: new Date(),
-      },
-    });
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
 
-    return response.data.user;
-  });
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
 
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(CreateUserFormSchema),
