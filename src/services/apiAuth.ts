7fcd2b57884b0a05cf1ec92c9a4e40f1121bfa22
Tuag <igortuag@gmail.com>
2022-07-24 06:26:42 -0300

    add interceptor to auth api

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,5 +1,23 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { parseCookies } from "nookies";
+const cookies = parseCookies();
 
 export const apiAuth = axios.create({
   baseURL: "http://localhost:3333/api",
+  headers: {
+    Authorization: `Bearer ${cookies["nextauth.token"]}`,
+  },
 });
+
+apiAuth.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+      } else {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
