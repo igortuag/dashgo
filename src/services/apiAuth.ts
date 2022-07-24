94aaea282548f1bfaf220c106fbd2a028571becc
Tuag <igortuag@gmail.com>
2022-07-24 06:30:39 -0300

    update token and refresh token

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from "axios";
-import { parseCookies } from "nookies";
-const cookies = parseCookies();
+import { parseCookies, setCookie } from "nookies";
+import { api } from "./api";
+let cookies = parseCookies();
 
 export const apiAuth = axios.create({
   baseURL: "http://localhost:3333/api",
@@ -14,6 +15,30 @@ apiAuth.interceptors.response.use(
   (error: AxiosError) => {
     if (error.response.status === 401) {
       if (error.response.data?.code === "token.expired") {
+        cookies = parseCookies();
+
+        const { "nextauth.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response.data;
+            setCookie(undefined, "nextauth", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: "/",
+            });
+            setCookie(
+              undefined,
+              "nextauth.refreshtoken",
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: "/",
+              }
+            );
+          });
       } else {
         window.location.href = "/login";
       }
