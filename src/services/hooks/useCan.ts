0176e56703b1e5e766bd4655ef23df7cedc81658
Tuag <igortuag@gmail.com>
2022-08-01 06:01:29 -0300

    add user context

--- src/services/hooks/useCan.ts
+++ src/services/hooks/useCan.ts
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
 type UseCanParams = {
   permissions?: string[];
   roles?: string[];
 };
 
 export default function useCan({ permissions, roles }: UseCanParams) {
+  const { user, isAuthenticated } = useContext(AuthContext);
+
   return {};
 }
