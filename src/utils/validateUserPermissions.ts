91dff89a085eae6450b5801300c46d683b820133
Tuag <igortuag@gmail.com>
2022-08-04 06:55:25 -0300

    separete function to validate user permissions

--- src/utils/validateUserPermissions.ts
+++ src/utils/validateUserPermissions.ts
@@ -13,4 +13,22 @@ export function validateUserPermissions({
   user,
   permissions,
   roles,
-}: validateUserPermissionsParams) {}
+}: validateUserPermissionsParams) {
+  if (permissions?.length > 0) {
+    const hasAllPermissions = permissions.every((permission) => {
+      return user.permissions.includes(permission);
+    });
+
+    if (!hasAllPermissions) return false;
+  }
+
+  if (roles?.length > 0) {
+    const hasAllPermissions = permissions.some((role) => {
+      return user.permissions.includes(role);
+    });
+
+    if (!hasAllPermissions) return false;
+  }
+
+  return true;
+}
